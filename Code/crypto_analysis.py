d8a3a154982b55daf6194e3f03eb0b18fa0cf517
Rtist <Rtist@users.noreply.github.com>
2018-03-17 17:45:20 +0100

    Update crypto_analysis.py

--- Code/crypto_analysis.py
+++ Code/crypto_analysis.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 
 
 ### retrieving data
-quandl.ApiConfig.api_key = 'rZENMjiDRz-bnbbPXSdy'
+quandl.ApiConfig.api_key = ''
 
 LTC_EUR = quandl.get("GDAX/LTC_EUR", authtoken="rZENMjiDRz-bnbbPXSdy")
 ETH_EUR = quandl.get("GDAX/ETH_EUR", authtoken="rZENMjiDRz-bnbbPXSdy")
